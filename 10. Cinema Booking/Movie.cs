b1dd56ce0a6068d44abe70575a79df479485b942
Ice713 <icramzilruiz@gmail.com>
2025-12-01 02:31:36 +0800

    Done with Movie and Ticket classes.
    working with even-handlers next.

---
+++ 10. Cinema Booking/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Cinema_Booking
+{
+    internal class Movie
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+        public Movie (string title, string genre    )
+        {
+            Title = title;
+            Genre = genre;
+        }
+
+        public virtual string ShowDetails()
+        {
+            return $"Movie: {Title}\nGenre: {Genre}";
+        }
+    }
+}
---
+++ 10. Cinema Booking/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Cinema_Booking
+{
+    internal class Ticket : Movie
+    {
+        public string Seat { get; set; }
+        public decimal Price { get; set; }
+
+        public Ticket(string title, string genre, string seat, decimal price) : base(title, genre)
+        {
+            Seat = seat;
+            Price = price;
+        }
+
+        public override string ShowDetails()
+        {
+            return base.ShowDetails() + $"Seat: {Seat}\nPrice: {Price}";
+        }
+    }
+}
